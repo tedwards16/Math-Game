ecbdb3ae6c786bc3c1e3b68333abb491c2892d38
tedwards16 <139355748+tedwards16@users.noreply.github.com>
2023-11-07 01:35:53 +0000

    Check answer works

--- script.js
+++ script.js
@@ -10,9 +10,7 @@ function equationGenerator () {
     var equation = {};
     var firstNumber = Math.floor(Math.random() * 10);
     var secondNumber = Math.floor(Math.random() * 10);
-    console.log(firstNumber);
-    console.log(secondNumber);
-    operator = Object.keys(operator)[Math.floor(Math.random() * 4)];
+    var operator = "+"
     while ((operator == "/" && secondNumber == 0) || (operator == "/" &&  firstNumber%secondNumber !== 0) || (operator == "-" && firstNumber < secondNumber)) {
       firstNumber = Math.floor(Math.random() * 10);
     }
@@ -20,6 +18,25 @@ function equationGenerator () {
     equation.rightSide = eval(equation.leftSide);
     return equation;
   }
-console.log(equationGenerator());
+  
+  var currentEquation = equationGenerator();
+
+  $("#equation").text(currentEquation.leftSide)
+
+  var correctAnswer = Number(currentEquation.rightSide);
+
+  function newEquation() {
+    currentEquation = equationGenerator();
+    $("#equation").text(currentEquation.leftSide);
+  }
+  $(".userInput").on('keyup', function() {
+    var userInput = $(".userInput").val();
+    if (correctAnswer == userInput) {
+      currentEquation = equationGenerator();
+      correctAnswer = Number(currentEquation.rightSide);
+      $("#equation").text(currentEquation.leftSide);
+      $(".userInput").val("");
+    }
+  });
 
 });
