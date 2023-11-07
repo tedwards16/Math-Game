09a138b1eae9661aa28c65859542500f2e60d0ec
tedwards16 <139355748+tedwards16@users.noreply.github.com>
2023-11-07 00:06:43 +0000

    equationGenerator working

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
-  
+  var operator = {
+    "+": 0,
+    "-": 1,
+    "*": 2,
+    "/": 3,
+  };
   
 function equationGenerator () {
     var equation = {};
@@ -7,7 +12,10 @@ function equationGenerator () {
     var secondNumber = Math.floor(Math.random() * 10);
     console.log(firstNumber);
     console.log(secondNumber);
-    var operator = ["*", "+", "-", "/"][Math.floor(Math.random() * 4)]; 
+    operator = Object.keys(operator)[Math.floor(Math.random() * 4)];
+    while ((operator == "/" && secondNumber == 0) || (operator == "/" &&  firstNumber%secondNumber !== 0) || (operator == "-" && firstNumber < secondNumber)) {
+      firstNumber = Math.floor(Math.random() * 10);
+    }
     equation.leftSide = firstNumber + operator + secondNumber;
     equation.rightSide = eval(equation.leftSide);
     return equation;
